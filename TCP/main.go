81a77d5ca5b77a74276c20cbd0e8bbaa0c9175fc
Aditya Singh <adityasingh27072746@gmail.com>
2026-06-04 02:15:23 +0530

    added small command parser

--- TCP/main.go
+++ TCP/main.go
@@ -4,42 +4,94 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
+	"sync"
 )
 
-func main() {
-	ln, err := net.Listen("tcp", ":9000")
+
+type Server struct {
+	mu      sync.Mutex   // to lock and unlock
+	counter int          // starting with something basic as counter will change it to byte in next stage of implementation
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Start(addr string) error {
+	ln, err := net.Listen("tcp", addr)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer ln.Close()
 
-	fmt.Println("Listening on :9000")
+	fmt.Println("Listening on", addr)
 
 	for {
 		conn, err := ln.Accept()
+		fmt.Println(conn)
 		if err != nil {
 			fmt.Println("accept error:", err)
 			continue
 		}
-
-		go handleConnection(conn)
+		go s.handleConnection(conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func (s *Server) handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	fmt.Println("Client connected:", conn.RemoteAddr())
 	scanner := bufio.NewScanner(conn)
 
 	for scanner.Scan() {
-		msg := scanner.Text()
-		fmt.Printf("[%s] %s\n", conn.RemoteAddr(), msg)
+		line := strings.TrimSpace(scanner.Text())
+		response := s.parseCommand(line)
+
+		_, err := fmt.Fprintln(conn, response)
+		if err != nil {
+			return
+		}
+		if response == "BYE" {
+			return
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("read error:", err)
 	}
-
-	fmt.Println("Client disconnected:", conn.RemoteAddr())
 }
+
+func (s *Server) parseCommand(line string) string {
+	fields := strings.Fields(line)
+
+	if len(fields) == 0 {
+		return "ERROR empty command"
+	}
+
+	cmd := strings.ToUpper(fields[0])
+	args := fields[1:]
+
+	switch cmd {
+
+	case "PING":
+		return "PONG"
+
+	case "ECHO":
+		return strings.Join(args, " ")
+
+	case "QUIT":
+		return "BYE"
+
+	default:
+		return "ERROR unknown command"
+	}
+}
+
+func main() {
+	server := NewServer()
+
+	if err := server.Start(":9000"); err != nil {
+		panic(err)
+	}
+}	
